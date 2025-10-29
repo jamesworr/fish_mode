d2282393c3be4c5b1a6701c6ffbfe0a8a25bee84
James Orr <jwillorr@gmail.com>
2025-10-28 21:03:05 -0500

    cleanup leftover junk

--- source/bowl.c
+++ source/bowl.c
@@ -13,35 +13,6 @@ OBJ_AFFINE *obj_aff_buffer= (OBJ_AFFINE*)obj_buffer;
 #define CBB_0  0
 #define SBB_0 28
 
-#define FPID 0
-#define WPID 1
-
-// [7:4] is Palette ID
-// [3:0] is Tile ID
-#define GRID_TL_W  0x30
-#define GRID_TR_W  0x31
-#define GRID_BL_W  0x33
-#define GRID_BR_W  0x32
-#define GRID_TL_G  0x50
-#define GRID_TR_G  0x51
-#define GRID_BL_G  0x53
-#define GRID_BR_G  0x52
-#define BLANK_BG   0x04
-
-#define BLOCK_WIDTH  16
-#define BLOCK_HEIGHT 16
-
-#define NUM_COLS 9
-#define NUM_ROWS 9
-#define MAX_BLOCKS 81 // FIXME
-#define BLOCK_GAP_WIDTH 0
-
-#define BLOCK_TILE_OFFSET 0
-
-//BG_POINT bg0_pt= { 0, 0 };
-//SCR_ENTRY *bg0_map= se_mem[SBB_0];
-
-
 void wait_any_key(void) {
     while(1) {
         vid_vsync();
@@ -91,11 +62,8 @@ void sprite_loop() {
 }
 
 int main() {
-    
-    // TODO sprite init
-    // Setup for tiled mode
-    //   into LOW obj memory (cbb == 4)
-    //  copy block
+
+    // Copy Sprite Tiles
     memcpy32(&tile_mem[4][0], fish_tiles, fish_tiles_len / sizeof(u32));
     memcpy16(pal_obj_mem, fish_pal, fish_pal_len / sizeof(u16));
     oam_init(obj_buffer, 128);
@@ -108,6 +76,7 @@ int main() {
     obj_set_pos(&obj_buffer[0], 120, 80);
     oam_copy(oam_mem, obj_buffer, 1);
 
+    // Copy background tiles
 	// Load palette
 	memcpy16(pal_bg_mem, bowl_bg_pal, bowl_bg_pal_len / sizeof(u16));
 	// Load tiles into CBB 0
@@ -116,7 +85,7 @@ int main() {
 	memcpy32(&se_mem[30][0], bowl_bg_map, bowl_bg_map_len / sizeof(u32));
 
     // Setup background and display registers
-    REG_BG0CNT  = BG_CBB(0) | BG_SBB(30) | BG_8BPP | BG_REG_64x32;
+    REG_BG0CNT  = BG_CBB(0) | BG_SBB(30) | BG_8BPP | BG_REG_32x32;
     REG_DISPCNT = DCNT_MODE0 | DCNT_BG0 | DCNT_OBJ | DCNT_OBJ_1D;
 
 
