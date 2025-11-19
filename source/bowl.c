901df5797bd9f81f397c4998e59d4378cd347f44
James Orr <jwillorr@gmail.com>
2025-11-18 23:08:04 -0600

    add fsm states for movement and acceleration

--- source/bowl.c
+++ source/bowl.c
@@ -24,6 +24,7 @@ typedef struct {
     // 3: accel negative
     u8 state;
 
+    // TODO switch to countdown??
     // index for v(t) function
     // always init to 0
     unsigned int count;
@@ -39,11 +40,96 @@ typedef struct {
 
 void update_fish_position(volatile fish_t* fish_ptr) {
     // TODO handle Y dimension
+    // FIXME redo count and use state
     if (fish_ptr->direction) {
         fish_ptr->x += fish_ptr->velocity_map[fish_ptr->count];
-    } else {
+    }
+    else {
         fish_ptr->x -= fish_ptr->velocity_map[fish_ptr->count];
     }
+}
+
+// 0: stationary
+void fish_fsm_0(volatile fish_t* fish_ptr) {
+    // TODO decide L/R priority
+    if (key_hit(KEY_LEFT)) {
+        fish_ptr->direction = 1;
+        fish_ptr->state = 2;
+    }
+    else if (key_hit(KEY_RIGHT)) {
+        fish_ptr->direction = 0;
+        fish_ptr->state = 2;
+    }
+    else {
+        fish_ptr->state = 0;
+    }
+}
+
+// 1: moving steady state
+void fish_fsm_1(volatile fish_t* fish_ptr) {
+    // skip to slow down when button released
+    if (key_released(KEY_LEFT | KEY_RIGHT)) {
+        fish_ptr->state = 3;
+    }
+    else {
+        fish_ptr->state = 1;
+    }
+}
+
+// 2: accel positive
+void fish_fsm_2(volatile fish_t* fish_ptr) {
+    // skip to slow down when button released
+    if (key_released(KEY_LEFT | KEY_RIGHT)) {
+        fish_ptr->state = 3;
+    }
+
+    // Check timer
+    if (fish_ptr->counter) {
+        fish_ptr->state = 2;
+    }
+    else {
+        // move to steady state after timer expire
+        fish_ptr->state = 1;
+    }
+}
+
+// 3: accel negative
+void fish_fsm_3(volatile fish_t* fish_ptr) {
+    // TODO button re-pressed
+    //if (key_pressed(KEY_LEFT | KEY_RIGHT)) {
+    //    fish_ptr->state = 2;
+    //}
+
+    // Check timer
+    if (fish_ptr->counter) {
+        fish_ptr->state = 3;
+    }
+    else {
+        // move to stationary after timer expire
+        fish_ptr->state = 0;
+    }
+}
+
+void update_fish_fsm(volatile fish_t* fish_ptr) {
+    // TODO handle Y dimension
+    switch (fish_ptr->state) {
+        case 0:
+            fish_fsm_0;
+            break;
+        case 1:
+            fish_fsm_1;
+            break;
+        case 2:
+            fish_fsm_2;
+            break;
+        case 3:
+            fish_fsm_3;
+            break;
+        default:
+            fish_fsm_0;
+            break;
+    }
+
 }
 
 void wait_any_key(void) {
@@ -83,18 +169,11 @@ void sprite_loop(volatile fish_t* fish_ptr) {
         if (key_is_down(KEY_DOWN)) {
             fish_ptr->y++;
         }
-        if (key_is_down(KEY_LEFT)) {
-            fish_ptr->x--;
-        }
-        if (key_is_down(KEY_RIGHT)) {
-            fish_ptr->x++;
-        }
 
         // TODO
         // update fish counter
-        // get new direction state from D pad
-        // handle new direction before current velocity decays fully
 
+        // update_fish_fsm(fish_ptr);
         // update_fish_position(fish_ptr);
 
         // copy to buffers
